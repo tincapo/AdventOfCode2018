c5a514f97046c93a4d21e4955613a115b44bc02c
Jacek Kruszynski <jacek@jaceks-mbp.home>
2018-12-02 19:04:13 +0000

    Day 1 - Task 1 - added a namespace due to errors in the Task 2

--- Day_1/task_1.ts
+++ Day_1/task_1.ts
@@ -1,25 +1,27 @@
-function processInput(inputStr: string): number {
-  const lineArray = inputStr.split("\n");
-  let sum: number = 0;
+namespace Task1 {
+  function processInput(inputStr: string): number {
+    const lineArray = inputStr.split("\n");
+    let sum: number = 0;
 
-  lineArray.forEach((line: string) => {
-    if (line[0] === "+") {
-      const value: number = parseInt(line.slice(1));
-      sum += value;
-    }
-    if (line[0] === "-") {
-      const value: number = parseInt(line.slice(1));
-      sum -= value;
-    }
-  });
+    lineArray.forEach((line: string) => {
+      if (line[0] === "+") {
+        const value: number = parseInt(line.slice(1));
+        sum += value;
+      }
+      if (line[0] === "-") {
+        const value: number = parseInt(line.slice(1));
+        sum -= value;
+      }
+    });
 
-  return sum;
+    return sum;
+  }
+
+  console.log("Answer:", processInput("+1\n+1\n+1"), "Expected:", 3);
+  console.log("Answer:", processInput("+1\n+1\n-2"), "Expected:", 0);
+  console.log("Answer:", processInput("-1\n-2\n-3"), "Expected:", -6);
+
+  let fs = require("fs");
+  let input = fs.readFileSync("./Day_1/input.txt", "utf8");
+  console.log("Answer:", processInput(input));
 }
-
-console.log("Answer:", processInput("+1\n+1\n+1"), "Expected:", 3);
-console.log("Answer:", processInput("+1\n+1\n-2"), "Expected:", 0);
-console.log("Answer:", processInput("-1\n-2\n-3"), "Expected:", -6);
-
-let fs = require("fs");
-let input = fs.readFileSync("./Day_1/input.txt", "utf8");
-console.log("Answer:", processInput(input));
