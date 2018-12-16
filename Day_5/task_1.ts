4e1c7075fc4435b67953b23af9651dc352c9f0f2
Jacek Kruszynski <jacek@jaceks-mbp.home>
2018-12-16 21:37:29 +0000

    Day 5 - Task 1 - solution

---
+++ Day_5/task_1.ts
@@ -0,0 +1,41 @@
+namespace Task2 {
+  function processInput(inputStr: string): number {
+    const input: string = inputStr.split("\n")[0];
+
+    let inputArr = input.split("");
+    let pairFound = true;
+
+    while (pairFound) {
+      pairFound = false;
+
+      inner_loop: for (let i = 0; i < inputArr.length - 1; i++) {
+        if (inputArr[i] === inputArr[i].toLowerCase()) {
+          if (inputArr[i] === inputArr[i + 1].toLowerCase()) {
+            if (inputArr[i + 1] === inputArr[i + 1].toUpperCase()) {
+              pairFound = true;
+              inputArr.splice(i, 2);
+              break inner_loop;
+            }
+          }
+        }
+        if (inputArr[i] === inputArr[i].toUpperCase()) {
+          if (inputArr[i] === inputArr[i + 1].toUpperCase()) {
+            if (inputArr[i + 1] === inputArr[i + 1].toLowerCase()) {
+              pairFound = true;
+              inputArr.splice(i, 2);
+              break inner_loop;
+            }
+          }
+        }
+      }
+    }
+
+    return inputArr.length;
+  }
+
+  console.log("Answer:", processInput("dabAcCaCBAcCcaDA"), "Expected:", 10);
+
+  let fs = require("fs");
+  let input = fs.readFileSync("./Day_5/input.txt", "utf8");
+  console.log("Answer:", processInput(input));
+}
